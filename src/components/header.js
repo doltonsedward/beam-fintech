0b977a4115c95d3c5766b0d03c70756071009be5
D3NCyan <edwarddoltons@gmail.com>
2022-01-05 17:07:11 +0800

    cleanup code in header

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,13 @@
 import * as React from "react"
 import PropTypes from "prop-types"
-import { Link } from "gatsby"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const Header = () => {
+  return (
+    <header>
+      <div className="text-3xl font-bold underline">Beam</div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
@@ -38,5 +16,4 @@ Header.propTypes = {
 Header.defaultProps = {
   siteTitle: ``,
 }
-
 export default Header
